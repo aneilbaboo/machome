60b28d7423e29320c2251ba00658dd8daa0ae510
Aneil Mallavarapu <aneil.mallavar@gmail.com>
2016-10-25 16:52:40 -0700

    Refactor & remove .bash_profile
    New approach is to source scripts located in the .machome/ folder.  That allows users
    to keep their existing .bash_profile and pick and choose which functionality to include.
    
    Simply add
    
    source ~/.machome/all.bash
    
    to get all of the machome features.

---
+++ .machome/all.bash
@@ -0,0 +1,31 @@
+export EDITOR=emacs
+export PATH="$PATH:/usr/local/bin:/usr/sbin:/bin:/sbin:/usr/bin:/usr/local/sbin"
+export PATH=${PATH}:~/bin
+export MANPATH=/usr/local/git/man:$MANPATH
+
+
+export PYTHONSTARTUP=~/.pythonrc.py # add readline capabilities to python commandline interpreter
+export PGDATA=/usr/local/var/postgres/
+
+source .machome/prompt.bash
+source .machome/machelp.bash
+source .machome/goconfig.bash
+source .machome/googlecloud.bash
+source .machome/rvm.bash
+source .machome/lunchy.bash
+source .machome/aquamacs.bash
+source .machome/colorman.bash
+source .machome/rails.bash
+source .machome/sshprompt.bash
+
+# bash completion
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
+
+pathadd() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        PATH="${PATH:+"$PATH:"}$1"
+    fi
+}
+
