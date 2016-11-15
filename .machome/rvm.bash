d1828639087ec57dced9c5637bb5c7e2ee2800dc
Aneil Mallavarapu <aneilmallavar@gmail.com>
2016-11-15 12:12:02 -0800

    Add .rvm/bin to path in rvm.bash

--- .machome/rvm.bash
+++ .machome/rvm.bash
@@ -1,4 +1,3 @@
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
-# Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" 
-
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
