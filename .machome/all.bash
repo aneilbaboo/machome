9f6598e4aa29ad38d8aeb1cbf1c7b53c9b277c6c
Aneil Mallavarapu <aneil.mallavar@gmail.com>
2016-11-16 13:38:18 -0800

    Fix all.bash

--- .machome/all.bash
+++ .machome/all.bash
@@ -7,17 +7,17 @@ export MANPATH=/usr/local/git/man:$MANPATH
 export PYTHONSTARTUP=~/.pythonrc.py # add readline capabilities to python commandline interpreter
 export PGDATA=/usr/local/var/postgres/
 
-source .machome/prompt.bash
-source .machome/machelp.bash
-source .machome/goconfig.bash
-source .machome/googlecloud.bash
-source .machome/rvm.bash
-source .machome/lunchy.bash
-source .machome/aquamacs.bash
-source .machome/colorman.bash
-source .machome/rails.bash
-source .machome/sshprompt.bash
-source .machome/bundler_bash_completion.bash
+source ~/.machome/prompt.bash
+source ~/.machome/machelp.bash
+source ~/.machome/goconfig.bash
+source ~/.machome/googlecloud.bash
+source ~/.machome/rvm.bash
+source ~/.machome/lunchy.bash
+source ~/.machome/aquamacs.bash
+source ~/.machome/colorman.bash
+source ~/.machome/rails.bash
+source ~/.machome/sshprompt.bash
+source ~/.machome/bundler_bash_completion.bash
 
 # bash completion
 if [ -f `brew --prefix`/etc/bash_completion ]; then
