d4c354d3060986b88a94984a21974bcd39817daa
Aneil Mallavarapu <aneil.mallavar@gmail.com>
2016-11-16 14:00:19 -0800

    Fix bundler_bash_completion problem

--- .machome/bundler_bash_completion.bash
+++ .machome/bundler_bash_completion.bash
@@ -1,4 +1,4 @@
-if [ -f `which complete_bundle_bash_command` ]; then
+if [ -f `which complete_bundle_bash_command` ] && [ "`gem list -i bundler_bash_completion`" == "true" ]; then
     eval "$(complete_bundle_bash_command init)"
 fi
 
