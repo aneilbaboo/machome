981c04f173749a0ba7171112cc8c7c295193ad63
Aneil Mallavarapu <aneilmallavar@gmail.com>
2017-04-04 04:07:08 -0700

    Add atom package installer script

---
+++ install-atom-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apm install git-plus
+apm install atom-beautify
+apm install linter
+apm install linter-eslint
+apm install node-debugger
+apm install node-resolver2
+apm install term3
